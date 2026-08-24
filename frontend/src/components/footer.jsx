9103992489a3c3208c99d488912b0365c46601e3
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-24 23:41:01 +0200

    mapp struktur och lite smått

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <footer>
+            <p>© 2026 Matte Experten. All rights reserved.</p>
+        </footer>
+    )
+}
+
+
+export default Footer;
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+    return(
+    <header>
+        <ul>
+            <h1>Matte Experten</h1>
+        </ul>
+    </header>
+)
+}
+
+
+
+
+export default Header;
+
+
+
+
+
+
