7900f4cdf187e0e84f590c2e29825a9e4bc17de5
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-25 00:09:29 +0200

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,16 @@
-import {useState} from 'react'
-
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
+import MathQuiz from './components/math.jsx';
 import './style.css'
 
 
 function App() {
-    const [answer, setAnswer] = useState("");
-    const [result, setResult] = useState("");
 
-    const correctAnswer = "2";
-
-    function checkAnswer(){
-        if (answer === correctAnswer){
-            setResult("Rätt");
-        } else {
-            setResult("Fel");
-        }
-    }
   return (
     <div>
       <Header />
       <main>
-            <section>
-                <h2>Lös ekvationen</h2>
-
-                <p>x² - 5x + 6 = 0</p>
-
-                <input
-                type='text'
-                value={answer}
-                onChange={(event) => setAnswer(event.target.value)}
-                placeholder='ditt svar'
-                />
-
-                <button onClick={checkAnswer}>
-                    Kontrollera
-                </button>
-
-                <p>{result}</p>
-            </section>
+        <MathQuiz/>
       </main>
       <Footer/>
     </div>
---
+++ frontend/src/components/math.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+function MathQuiz() {
+  const [answer, setAnswer] = useState("");
+  const [result, setResult] = useState("");
+
+
+
+
+  const questions = [
+    {
+        text: "x² - 5x + 6 = 0",
+        answer: ["2", "3"],
+        category: "Andragradsekvationer",
+        difficulty: "medium",
+    },
+    {
+        text: "2x + 7 = 15",
+        answer: ["4"],
+        category: "Linjära ekvationer",
+        difficulty: "easy",
+    },
+];
+
+    const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+
+    const correctAnswer = randomQuestion.answer;
+  function checkAnswer() {
+    if (answer === correctAnswer) {
+      setResult("Rätt");
+    } else {
+      setResult("Fel");
+    }
+  }
+
+  console.log(correctAnswer);
+  console.log(randomQuestion);
+
+  return (
+    <section>
+      <h2>Lös ekvationen</h2>
+
+      <p>{randomQuestion.text}</p>
+
+      <input
+        type="text"
+        value={answer}
+        onChange={(event) => setAnswer(event.target.value)}
+        placeholder="ditt svar"
+      />
+
+      <button onClick={checkAnswer}>Kontrollera</button>
+
+      <p>{result}</p>
+    </section>
+  );
+}
+
+export default MathQuiz;
