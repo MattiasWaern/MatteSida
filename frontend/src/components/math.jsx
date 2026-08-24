aa869d1823b0d92ea7cee8f8a1652fe2ef2fe5be
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-25 00:27:55 +0200

    Update math.jsx

--- frontend/src/components/math.jsx
+++ frontend/src/components/math.jsx
@@ -2,39 +2,53 @@ import { useState } from "react";
 import '../styles/math.css'
 
 function MathQuiz() {
-  const [answer, setAnswer] = useState("");
+  const [answer1, setAnswer1] = useState("");
+  const [answer2, setAnswer2] = useState("");
   const [result, setResult] = useState("");
-
-
-
 
   const questions = [
     {
         text: "x² - 5x + 6 = 0",
         answer: ["2", "3"],
+        type: "multiple",
         category: "Andragradsekvationer",
         difficulty: "medium",
     },
     {
         text: "2x + 7 = 15",
         answer: ["4"],
+        type: "single",
         category: "Linjära ekvationer",
         difficulty: "easy",
     },
 ];
 
-    const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+  const [randomQuestion, setRandomQuestion] = useState(
+        questions[Math.floor(Math.random() * questions.length)]);
 
-    const correctAnswer = randomQuestion.answer;
-  function checkAnswer() {
-    if (answer === correctAnswer) {
-      setResult("Rätt");
-    } else {
-      setResult("Fel");
+
+    function checkAnswer() {
+        if (randomQuestion.type === 'single') {
+            if(answer1 === randomQuestion.answer[0]){
+                setResult("Rätt!");
+            }
+        } else {
+            setResult("Fel!");
+        }
+
+        if(randomQuestion.type === 'multple'){
+            const userAnswers = [answer1, answer2].sort();
+            const correctAnswers = [...randomQuestion.answer].sort();
+
+            if(JSON.stringify(userAnswers) === JSON.stringify(correctAnswers)){
+                setResult("Rätt")
+            } else {
+                setResult("Fel");
+            }
+        }
     }
-  }
 
-  console.log(correctAnswer);
+
   console.log(randomQuestion);
 
   return (
@@ -45,12 +59,19 @@ function MathQuiz() {
 
       <p>{randomQuestion.text}</p>
 
-      <input
-        type="text"
-        value={answer}
-        onChange={(event) => setAnswer(event.target.value)}
-        placeholder="ditt svar"
-      />
+        <input
+            type="text"
+            value={answer1}
+            onChange={(event) => setAnswer1(event.target.value)}
+            placeholder="x₁"
+        />
+
+        <input
+            type="text"
+            value={answer2}
+            onChange={(event) => setAnswer2(event.target.value)}
+            placeholder="x₂"
+        />
 
       <button onClick={checkAnswer}>Kontrollera</button>
 
