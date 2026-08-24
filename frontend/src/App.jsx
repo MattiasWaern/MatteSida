40206efe1cb340b3f7312068376519f1ef2dc944
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-24 23:51:30 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,45 @@
+import {useState} from 'react'
+
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
 import './style.css'
 
 
 function App() {
+    const [answer, setAnswer] = useState("");
+    const [result, setResult] = useState("");
+
+    const correctAnswer = "2";
+
+    function checkAnswer(){
+        if (answer === correctAnswer){
+            setResult("Rätt");
+        } else {
+            setResult("Fel");
+        }
+    }
   return (
     <div>
       <Header />
       <main>
-        <h2>Welcome to My App</h2>
-        <p>This is the main content area.</p>
+            <section>
+                <h2>Lös ekvationen</h2>
+
+                <p>x² - 5x + 6 = 0</p>
+
+                <input
+                type='text'
+                value={answer}
+                onChange={(event) => setAnswer(event.target.value)}
+                placeholder='ditt svar'
+                />
+
+                <button onClick={checkAnswer}>
+                    Kontrollera
+                </button>
+
+                <p>{result}</p>
+            </section>
       </main>
       <Footer/>
     </div>
