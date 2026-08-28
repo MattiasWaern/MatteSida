cee41ed9c156751fbda8aa96d60a977c72e96478
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-08-28 17:13:29 +0200

    c

--- frontend/src/pages/math.jsx
+++ frontend/src/pages/math.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import questions from "../data/questions.json";
+import questions from "../data/index.js";
 import GeometryRender from "../components/geometryRender";
 import StatisticsRender from "../components/statisticsRender";
 import "../styles/math.css";
