04766164bd3c3bde081a15f37db7bfa23bbaa75e
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-03 14:40:37 +0200

    Update prov.jsx

--- frontend/src/pages/prov.jsx
+++ frontend/src/pages/prov.jsx
@@ -15,6 +15,45 @@ function Prov(){
     const [markedQuestions, setMarkedQuestions] = useState([]);
 
     const [timeLeft, setTimeLeft] = useState(60 * 60); // 60 minuter
+
+
+
+    function startExam(){
+        const shuffledQuestions = [...questions]
+            .sort(() => Math.random() - 0.5)
+            .slice(0, 20);
+
+            setExamQuestions(shuffledQuestions);
+            setCurrentQuestion(0);
+            setAnswers({});
+            setMarkedQuestions([]);
+            setTimeLeft(60 * 60);
+
+            setStarted(true);
+            setFinished(false);
+    }
+
+    useState(() => {
+        if(!started || finished) return;
+
+        if(timeLeft <=0){
+            submitExam();
+            return;
+        }
+
+        const timer = setInterval(() => {
+            setTimeLeft((time) => time  1);
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, [started, finished, timeLeft]);
+
+    function formatTime(seconds){
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+
+        return `${String(minutes).padStart(2, "0")}:${String(remainingSeconds).padStart(2, "0")}`;
+    }
 }
 
 
