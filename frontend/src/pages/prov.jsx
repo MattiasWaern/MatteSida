9ebcd8c0118df11f3eaa47bf4f9ea674ce882441
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-03 14:19:25 +0200

    variabler

--- frontend/src/pages/prov.jsx
+++ frontend/src/pages/prov.jsx
@@ -5,7 +5,16 @@ import statisticsRender from "../components/statisticsRender";
 import "../styles/prov.css"
 
 function Prov(){
-    
+    const [started, setStarted] = useState(false);
+    const [finished, setFinished] = useState(false);
+
+    const [examQuesions, setExamQuestions] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+
+    const [answers, setAnswers] = useState({});
+    const [markedQuestions, setMarkedQuestions] = useState([]);
+
+    const [timeLeft, setTimeLeft] = useState(60 * 60); // 60 minuter
 }
 
 
