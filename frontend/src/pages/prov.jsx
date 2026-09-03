2ce8797fcd9e8b441ba9a885e6ed640adb3f314b
ReggeMeister <80971552+MattiasWaern@users.noreply.github.com>
2026-09-03 14:17:05 +0200

    imports

--- frontend/src/pages/prov.jsx
+++ frontend/src/pages/prov.jsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from "react";
+import questions from "../data";
+import GeometryRender from "../components/geometryRender";
+import statisticsRender from "../components/statisticsRender";
+import "../styles/prov.css"
+
 function Prov(){
-    return(
-        <h1>Här är mitt Prov</h1>
-    )
+    
 }
 
 
